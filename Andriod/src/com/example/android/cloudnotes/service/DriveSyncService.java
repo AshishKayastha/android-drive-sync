4f6c5b174d988ebbfaecab70ee7779535de9b9ed
Nick Butcher <nickbutcher@gmail.com>
2012-11-12 06:35:15 +0100

    Step 5 complete

---
+++ Andriod/src/com/example/android/cloudnotes/service/DriveSyncService.java
@@ -0,0 +1,14 @@
+package com.example.android.cloudnotes.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class DriveSyncService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+}
